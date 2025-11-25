473f8f2185cb785621e5e6581a1ba3e35f70c630
hppys <carlos.sirait.1603@gmail.com>
2025-11-25 21:21:34 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import hotelRoutes from "./routes/hotelRoutes.js";
 
 dotenv.config();
 
 const app = express();
+
+// Tambahkan CORS
+app.use(
+  cors({
+    origin: "*", // atau "http://localhost:5173" saja
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 
-// Base route
+// Base test route
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
@@ -15,5 +26,4 @@ app.get("/", (req, res) => {
 // API routes
 app.use("/api/hotels", hotelRoutes);
 
-// ❗ WAJIB untuk Vercel: export default, BUKAN listen()
 export default app;
